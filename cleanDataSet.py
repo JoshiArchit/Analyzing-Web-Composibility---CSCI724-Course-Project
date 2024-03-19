2d9a1f1e8f5668d3451a64b21d3cc142db3085ea
architJoshi2904 <aj6082@rit.edu>
2024-03-19 19:21:44 -0400

    cleanDataSet.py v1

---
+++ cleanDataSet.py
@@ -0,0 +1,41 @@
+"""
+Filename : cleanDataSet
+Author : Archit
+Date Created : 3/19/2024
+Description : 
+Language : python3
+"""
+import os
+import json
+from extractFeatures import extractFeatures
+from getServer import get_server
+from checkNumPaths import get_num_paths
+
+
+def do_the_thing():
+    valid_file_name = 'final_valid_APIs.txt'
+    # Clear the file
+    with open(valid_file_name, 'w') as valid_file:
+        valid_file.truncate(0)
+
+    invalid_file_count = 0
+
+    folder_path = 'APIsGuru'
+    # Iterate through all the files in APIsGuru folder
+    for file in os.listdir(folder_path):
+        if 'json' in file:
+            file_name = "APIsGuru/" + file
+            with open(file_name, 'r', encoding='utf-8') as api_file:
+                data = json.load(api_file)
+                if extractFeatures(data) and get_server(data) and get_num_paths(data):
+                    with open(valid_file_name, 'a') as valid_file:
+                        valid_file.write(file + '\n')
+
+                else:
+                    invalid_file_count += 1
+
+    print("Invalid file count: ", invalid_file_count)
+
+
+if __name__ == '__main__':
+    do_the_thing()
