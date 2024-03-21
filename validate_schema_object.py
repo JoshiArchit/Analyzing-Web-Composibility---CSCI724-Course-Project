f9b9598fe4a243e881e27903eb21dbe9effecfd0
Chung-An Huang <chungan2@illinois.edu>
2024-03-21 17:32:19 -0400

    added schema object validation

---
+++ validate_schema_object.py
@@ -0,0 +1,63 @@
+
+
+def validate_schema(json_object):
+    is_valid_schema = True
+
+    P_J = ["title", "multipleOf", "maximum", "exclusiveMaximum", "minimum", "exclusiveMinimum", "maxLength", "minLength", "pattern", "maxItems", "minItems", "uniqueItems", "maxProperties", "minProperties", "required", "enum"]
+    P_A = ["type", "allOf", "oneOf", "anyOf", "not", "items", "properties", "additionalProperties", "description", "format", "default"]
+
+    for key in json_object:
+        if key in P_J or key in P_A:
+            value = json_object[key]
+
+            if isinstance(value, (int, float)):
+                if key in P_J and key not in ["title", "pattern", "required", "enum"]:
+                    continue
+                else:
+                    is_valid_schema = False
+                    break
+            elif isinstance(value, str):
+                if key in ["title", "pattern", "type", "format", "description"]:
+                    continue
+                else:
+                    is_valid_schema = False
+                    break
+            elif isinstance(value, list):
+                is_simple_array = True
+                for item in value:
+                    if not isinstance(item, (int, float, str)):
+                        is_simple_array = False
+                        break
+                if is_simple_array:
+                    if key in ["required", "enum"]:
+                        continue
+                    else:
+                        is_valid_schema = False
+                        break
+                else:
+                    if key in P_A and key not in ["type", "format", "description"]:
+                        continue
+                    else:
+                        is_valid_schema = False
+                        break
+            elif isinstance(value, dict):
+                if key in ["properties", "items"]:
+                    continue
+                else:
+                    is_valid_schema = False
+                    break
+            else:
+                is_valid_schema = False
+                break
+        else:
+            is_valid_schema = False
+            break
+    
+    return is_valid_schema
+
+def main():
+    test_schema = {"title": 2.0}
+    print(validate_schema(test_schema))
+
+if __name__ == "__main__":
+    main()
