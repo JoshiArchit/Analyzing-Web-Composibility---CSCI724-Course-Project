0c30c757abec2dde69b040d735abf3f8ec72c11d
architJoshi2904 <aj6082@rit.edu>
2024-03-21 22:08:33 -0400

    Bug Fixes & Docstrings

--- extractMethodParameters.py
+++ extractMethodParameters.py
@@ -1,6 +1,6 @@
 """
 Filename : extractMethodParameters
-Author : Archit ... but more like ** Athina **  :)
+Author : Archit Joshi (aj6082)
 Date Created : 3/20/2024
 Description : Script to extract the get/post parameters from the API file.
 Language : python3
@@ -11,26 +11,25 @@ from database import get_collection, post_collection
 
 
 def getParameters(api, path_dictionary):
+    """
+    Extracts the parameters from the API file for the get request. exclude_keys_list contains the
+    keys that are not to be extracted as per the authors are they introduce inconsistencies in
+    the data.
+
+    :param api: openapi/swagger api specification file.
+    :param path_dictionary: contents of the path attribute in the api file.
+    :return: dictionary with parameters for the get request.
+    """
     doc = dict()
     if 'parameters' in path_dictionary['get']:
         list_of_parameters = path_dictionary['get']['parameters']
+
+        # List of keys that are not to be extracted
         exclude_keys_list = [
-            "description",
-            "title",
-            "default",
-            "example",
-            "readOnly",
-            "writeOnly",
-            "example",
-            "title",
-            "enum",
-            "required",
-            "minimum",
-            "maximum",
-            "multipleOf",
-            "minItems",
-            "maxItems"
+            "description", "title", "default", "example", "readOnly", "writeOnly", "example",
+            "title", "enum", "required", "minimum", "maximum", "multipleOf", "minItems", "maxItems"
         ]
+
         # Iterate through the list of parameters
         for i, parameter in enumerate(list_of_parameters):
             operation = dict()
@@ -78,30 +77,29 @@ def getParameters(api, path_dictionary):
                     # Skip the parameter and continue
                     continue
             doc[f"get_{i}_parameters"] = operation
+
+    # Check for empty keys that will be filtered out later
     if len(doc) == 0:
         return 0
     else:
         return doc
 
 
-def postParameters(api, path_dictionary, parameter_type):
+def postParameters(api, path_dictionary):
+    """
+    Extracts the parameters from the API file for the post request. exclude_keys_list contains
+    the keys that are not to be extracted as per the authors are they introduce inconsistencies
+    in the data. Extraction needs to be performed for parameters sent as a part of the url as
+    well as the http request body.
+
+    :param api: openapi/swagger api specification file.
+    :param path_dictionary: contents of the path attribute in the api file.
+    :return: dictionary with parameters for the get request.
+    """
     doc = dict()
     exclude_keys_list = [
-        "description",
-        "title",
-        "default",
-        "example",
-        "readOnly",
-        "writeOnly",
-        "example",
-        "title",
-        "enum",
-        "required",
-        "minimum",
-        "maximum",
-        "multipleOf",
-        "minItems",
-        "maxItems"
+        "description", "title", "default", "example", "readOnly", "writeOnly", "example",
+        "title", "enum", "required", "minimum", "maximum", "multipleOf", "minItems", "maxItems"
     ]
     operation_number = 0
     if 'parameters' in path_dictionary['post']:
@@ -192,6 +190,7 @@ def postParameters(api, path_dictionary, parameter_type):
             except KeyError as e:
                 # Skip the parameter and continue
                 pass
+    # Check for empty keys that will be filtered out later
     if len(doc) == 0:
         return 0
     else:
@@ -205,13 +204,15 @@ def extractParameters(api_file):
     :param api_file: api file from which the parameters are to be extracted
     :return: document with parameters for each path
     """
-    doc = dict()
     get_doc = dict()
     post_doc = dict()
 
     path_list = api_file['paths']
+
     for path in path_list:
         method_type = path_list[path]
+
+        # Check if the get or post request is present in the path
         if 'get' in method_type:
             get_doc[path] = dict()
             dict_of_parameters = getParameters(api_file, method_type)
@@ -221,37 +222,28 @@ def extractParameters(api_file):
             else:
                 get_doc.pop(path)
         if 'post' in method_type:
-            if 'parameters' in method_type['post']:
-                dict_of_parameters = postParameters(api_file, method_type, 'parameters')
+            # Check if the post request is present in the path
+            if 'parameters' or 'requestBody' in method_type['post']:
+                dict_of_parameters = postParameters(api_file, method_type)
                 post_doc[path] = dict_of_parameters
-                # if the list_of_parameters is not empty, add it to the doc
-                # if dict_of_parameters != 0:
-                #     post_doc[path] = dict_of_parameters
-                # else:
-                #     post_doc.pop(path)
-            elif 'requestBody' in method_type['post']:
-                dict_of_parameters = postParameters(api_file, method_type, 'requestBody')
-                post_doc[path] = dict_of_parameters
-                # if the list_of_parameters is not empty, add it to the doc
-                # if dict_of_parameters != 0:
-                #     post_doc[path] = dict_of_parameters
-                # else:
-                #     print(path)
-                #     post_doc.pop(path)
+
+            # elif 'requestBody' in method_type['post']:
+            #     dict_of_parameters = postParameters(api_file, method_type)
+            #     post_doc[path] = dict_of_parameters
+
             # Remove all the keys with a 0 value
             post_doc = {k: v for k, v in post_doc.items() if v}
     return get_doc, post_doc
 
 
 def unit_test_get_document():
+    """
+    Unit test for the extractParameters function.
+    :return: None
+    """
     get_file = 'APIsGuru/openapi.json.2'
     with open(get_file, 'r', encoding='utf-8') as api_file:
         get_data = json.load(api_file)
         get_document, post_document = extractParameters(get_data)
         print(get_document)
         print(post_document)
-
-
-# unit_test()
-unit_test_get_document()
-
